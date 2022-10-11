f1d3696f0e0e54e2bb23f6494277ffc374054165
atikhasanps <atikhasanps@gmail.com>
2022-10-11 21:57:02 +0600

    quiz option

--- src/components/QuizShow/QuizShow.js
+++ src/components/QuizShow/QuizShow.js
@@ -1,21 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const QuizShow = ({quiz}) => {
     const{question,id,options, name } = quiz 
     
     return (
-        <div className='w-1/2 mx-auto mt-12 bg-orange-400 rounded-2xl'>
-                            <h2>Quiz of{name}</h2>
-            <div className=''>
-                <h2 className='text-2xl py-4 w-3/4 mx-auto'>Quiz: {question}</h2>
-                <div className='grid grid-cols-2 gap-4 w-3/4 mx-auto pb-4'>
-                <div><p className='border border-solid border-2 border-slate-400 rounded-lg p-4'>{options[0]}</p></div>
-                <div><p className='border border-solid border-2 border-slate-400 rounded-lg p-4'>{options[1]}</p></div>
-                <div><p className='border border-solid border-2 border-slate-400 rounded-lg p-4'>{options[2]}</p></div>
-                <div><p className='border border-solid border-2 border-slate-400 rounded-lg p-4'>{options[3]}</p></div>
-                </div>
-            </div>
-        </div>
+
+            
+
+                    
+            <div className='w-1/2 mx-auto mt-12 bg-orange-400 rounded-2xl'>
+                            
+                            <div className=''>
+
+                                <h2 className='text-2xl py-4 w-3/4 mx-auto'>Quiz: {question}</h2>
+
+                                <div className='grid grid-cols-2 gap-4 w-3/4 mx-auto pb-4'>
+
+                                <div className='border border-solid border-2 border-slate-400 rounded-lg p-4'><input className='rounded-full border-2 w-6 h-6 ' type="radio" name="fgf"/> <label>{options[0]}</label></div>
+
+                                
+                                <div className='border border-solid border-2 border-slate-400 rounded-lg p-4'><input className='rounded-full border-2 w-6 h-6 ' type="radio" name="fgf"/> <label>{options[1]}</label></div>
+
+                                <div className='border border-solid border-2 border-slate-400 rounded-lg p-4'><input className='rounded-full border-2 w-6 h-6 ' type="radio" name="fgf"/> <label>{options[2]}</label></div>
+                                   
+
+                                <div className='border border-solid border-2 border-slate-400 rounded-lg p-4'><input className='rounded-full border-2 w-6 h-6 ' type="radio" name="fgf"/> <label>{options[3]}</label></div>
+
+                               
+                               
+
+
+                            
+
+
+
+
+                            </div>
+                        </div>
+
+               </div>
+        
     );
 };
 

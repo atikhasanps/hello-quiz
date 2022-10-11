8d95c76dfc7f42c78b3b4a6fca4cd29029aae65e
atikhasanps <atikhasanps@gmail.com>
2022-10-11 19:51:40 +0600

    quiz question

--- src/components/QuizShow/QuizShow.js
+++ src/components/QuizShow/QuizShow.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const QuizShow = ({quiz}) => {
-    const{question,id,options } = quiz
+    const{question,id,options, name } = quiz 
+    
     return (
         <div className='w-1/2 mx-auto mt-12 bg-orange-400 rounded-2xl'>
+                            <h2>Quiz of{name}</h2>
             <div className=''>
                 <h2 className='text-2xl py-4 w-3/4 mx-auto'>Quiz: {question}</h2>
-                <div className='grid grid-cols-2 w-3/4 mx-auto pb-4'>
+                <div className='grid grid-cols-2 gap-4 w-3/4 mx-auto pb-4'>
                 <div><p className='border border-solid border-2 border-slate-400 rounded-lg p-4'>{options[0]}</p></div>
                 <div><p className='border border-solid border-2 border-slate-400 rounded-lg p-4'>{options[1]}</p></div>
                 <div><p className='border border-solid border-2 border-slate-400 rounded-lg p-4'>{options[2]}</p></div>
